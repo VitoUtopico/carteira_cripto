5545cef1adcb27ba3804deedb76ea4d4cd4150c7
VitoUtopico <fabiovito@hotmail.com>
2022-02-28 18:09:15 -0300

    Adição Resource ReadCryptocurrency
    Adição de classe para listagem de todas moedas registradas no banco
    TODO: Adaptar para retornar apenas as cryptocurrencies relacionadas
    ao User logado

--- carteira_cripto/blueprints/restapi/__init__.py
+++ carteira_cripto/blueprints/restapi/__init__.py
@@ -1,13 +1,17 @@
 from importlib import resources
+from mimetypes import init
 from attr import validate
 from flask import Blueprint
 from flask_restful import Api
 
-from .resources import WalletCalc
+from .resources import WalletCalc, ReadCryptocurrency
 
-bp = Blueprint('restapi', __name__, url_prefix='/api/v1')
+bp = Blueprint('restapi',\
+                __name__,
+                url_prefix='/api/v1')
 api = Api(bp)
 api.add_resource(WalletCalc, '/calcs/')
+api.add_resource(ReadCryptocurrency, '/criptos/')
 
 
 def init_app(app):
--- carteira_cripto/blueprints/restapi/resources.py
+++ carteira_cripto/blueprints/restapi/resources.py
@@ -2,7 +2,16 @@ from flask import abort, jsonify, request
 from flask_restful import Resource
 from carteira_cripto.ext.database.models import *
 
+
 class WalletCalc(Resource):
     def post(self):
         name = request.form['name']
         return(name)
+
+class ReadCryptocurrency(Resource):
+    def get(self):
+        cryptos = Cryptocurrency.query.all() or abort(204)
+        print(cryptos)
+        return jsonify(
+            {'Cryptocurrency': [crypto.to_dict() for crypto in cryptos]}
+        )
