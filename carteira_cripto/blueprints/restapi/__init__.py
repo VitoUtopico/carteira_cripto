af8d19ac58fc8c1e28e2f88c79932dc1658be86e
VitoUtopico <fabiovito@hotmail.com>
2022-03-01 11:16:49 -0300

    Adição Resources para inserção de dados no banco
    Foi construída uma classe de um Resource post para cada tabela
    do banco com um método de inserção de dados

--- carteira_cripto/blueprints/restapi/__init__.py
+++ carteira_cripto/blueprints/restapi/__init__.py
@@ -4,14 +4,23 @@ from attr import validate
 from flask import Blueprint
 from flask_restful import Api
 
-from .resources import WalletCalc, ReadCryptocurrency
+from .resources import WalletCalc,\
+                        ListCryptocurrency,\
+                        InsertCryptocurrency,\
+                        InsertWallet,\
+                        InsertUser,\
+                        InsertCryptocurrencyWallet
 
 bp = Blueprint('restapi',\
                 __name__,
                 url_prefix='/api/v1')
 api = Api(bp)
 api.add_resource(WalletCalc, '/calcs/')
-api.add_resource(ReadCryptocurrency, '/criptos/')
+api.add_resource(ListCryptocurrency, '/cryptos/')
+api.add_resource(InsertCryptocurrency, '/insertcrypto/')
+api.add_resource(InsertWallet, '/insertwallet/')
+api.add_resource(InsertUser, '/insertuser/')
+api.add_resource(InsertCryptocurrencyWallet, '/insertcryptowallet/')
 
 
 def init_app(app):
--- carteira_cripto/blueprints/restapi/resources.py
+++ carteira_cripto/blueprints/restapi/resources.py
@@ -8,10 +8,61 @@ class WalletCalc(Resource):
         name = request.form['name']
         return(name)
 
-class ReadCryptocurrency(Resource):
+class ListCryptocurrency(Resource):
     def get(self):
-        cryptos = Cryptocurrency.query.all() or abort(204)
+        # cryptos = Cryptocurrency.query.all() or abort(204)
+        # cryptos = Wallet.query.all() or abort(204)
+        # cryptos = User.query.all() or abort(204)
+        cryptos = CryptocurrencyWallet.query.all() or abort(204)
         print(cryptos)
         return jsonify(
             {'Cryptocurrency': [crypto.to_dict() for crypto in cryptos]}
         )
+
+class InsertCryptocurrency(Resource):
+    def post(self):
+        data = [
+                    Cryptocurrency(
+                        nm_cryptocurrency = request.form['nm_cryptocurrency'],
+                        cd_cryptocurrency = request.form['cd_cryptocurrency']
+                    )
+                ]
+        db.session.bulk_save_objects(data)
+        db.session.commit()
+        
+
+class InsertWallet(Resource):
+    def post(self):
+        data = [
+                    Wallet(
+                        id_user = request.form['id_user'],
+                        nm_wallet = request.form['nm_wallet']
+                    )
+                ]
+        db.session.bulk_save_objects(data)
+        db.session.commit()
+
+class InsertUser(Resource):
+    def post(self):
+        data = [
+                    User(
+                        nm_username = request.form['nm_username'],
+                        cd_password = request.form['cd_password']
+                    )
+                ]
+        db.session.bulk_save_objects(data)
+        db.session.commit()
+
+class InsertCryptocurrencyWallet(Resource):
+    def post(self):
+        data = [
+                    CryptocurrencyWallet(
+                        id_wallet= request.form['id_wallet'],
+                        id_cryptocurrency = request.form['id_cryptocurrency'],
+                        nr_current_amount = request.form['nr_current_amount'],
+                        nr_rating = request.form['nr_rating']
+                    )
+                ]
+        db.session.bulk_save_objects(data)
+        db.session.commit()
+        
